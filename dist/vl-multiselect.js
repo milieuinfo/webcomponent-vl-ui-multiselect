88028e7fc21c3ae7fa2ac10adae5afe53a23c8a3
Tom Coemans <tom.coemans@gmail.com>
2020-10-16 10:22:44 +0200

    dist build

--- dist/vl-multiselect.js
+++ dist/vl-multiselect.js
@@ -24,13 +24,9 @@ export class VlMultiSelect extends VlSelect {
     return 'VlMultiSelectReady';
   }
 
-  constructor() {
-    super();
-    this.setAttribute('data-vl-multiselect', '');
-  }
-
   connectedCallback() {
     this.classList.add('vl-multiselect');
+    this.setAttribute('data-vl-multiselect', '');
     this.setAttribute('name', 'multiselect');
     super.connectedCallback();
   }
