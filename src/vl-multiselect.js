bf14bf32df4274ba88cbcd7ded399296eab1d4b2
Gert-Jan Meire <gertjan.meire@gmail.com>
2021-06-17 16:24:12 +0200

    Update vl-ui-util.

--- src/vl-multiselect.js
+++ src/vl-multiselect.js
@@ -1,5 +1,5 @@
-import { define } from '/node_modules/vl-ui-core/dist/vl-core.js';
-import { VlSelect } from '/node_modules/vl-ui-select/dist/vl-select.js';
+import {define} from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {VlSelect} from '/node_modules/vl-ui-select/dist/vl-select.js';
 
 /**
  * VlMultiSelect
@@ -33,13 +33,13 @@ export class VlMultiSelect extends VlSelect {
     super.connectedCallback();
   }
 
-  hasSelected = () => {
+  hasSelected() {
     const options = Array.from(this.querySelectorAll('option'));
 
     return options.some((option) => {
       return option.hasAttribute('selected');
     });
-  };
+  }
 
   /**
    * Geeft de ready event naam.
@@ -89,6 +89,4 @@ export class VlMultiSelect extends VlSelect {
   }
 }
 
-window.customElements
-  .whenDefined('vl-select')
-  .then(() => define('vl-multiselect', VlMultiSelect, { extends: 'select' }));
+window.customElements.whenDefined('vl-select').then(() => define('vl-multiselect', VlMultiSelect, {extends: 'select'}));
